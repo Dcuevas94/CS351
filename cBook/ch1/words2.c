51ec4e828e792f38043f6c0f7ce61debfe5c59c0
Daniel Cuevas <dcuevas@hawk.iit.edu>
2015-01-16 22:13:40 -0600

    doesn't count empty spaces as chars

---
+++ cBook/ch1/words2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define IN 1	//inside a word
+#define OUT 0 	//outside a word
+
+//count lines, words and characters in the input
+int main()
+{
+	int c, nl, nw, nc, state;
+
+	state = OUT;
+	nl = nw = nc = 0;
+	while((c = getchar()) != '0'){
+		if (c == ' ')	//don't count blanks as nc
+			;
+		else if(1){		//increas nc otherwise
+			++nc;
+		}
+		if ( c == '\n')
+			++nl;
+		if ( c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+		else if (state == OUT){
+			state = IN;
+			++nw;
+		}
+	}
+	printf("%d (new line) %d (new word) %d (new char)\n", nl, nw, nc);
+}
